eba6f9ca762abcc41d5929e9cadba3b8455da4e4
ryanoligen <ryanoligen@gamil.com>
2021-02-24 17:17:23 +0800

    finish this course

--- get-quote.py
+++ get-quote.py
@@ -4,13 +4,18 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  # f = open("quotes.txt", "a")
+  # f.write(" Hello Python")
+  # f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  f = open('quotes.txt')
+  for i in f.readlines():
+    i = i.strip('\n')
+    print(i)
+
+  # last = 13
+  # rnd = random.randint(0, last)
+  # print(quotes[rnd])
 
 if __name__== "__main__":   
   primary()
